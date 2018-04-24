5f9a8ec268c3c22a2caf2c6a364b473e8fbada24
markcarrmusic@gmail.com <markcarrmusic@gmail.com>
2018-04-24 03:43:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,8 +62,10 @@ function placeOrder(cardNumber) {
     cardEntered = false;
   }
   if(cardEntered === true) {
-    return ('Your total cost is $' + total() + `, which will be charged to the card ${cardNumber}.`);
+    var cardText = [];
+    cardText.push('Your total cost is $' + total() + `, which will be charged to the card ${cardNumber}.`);
     cart.length = 0;
+    return cardText;
   } else {
     return "Sorry, we don't have a credit card on file for you."
   }
